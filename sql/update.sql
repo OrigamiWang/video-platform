c3e5edd19f027ceb73550fb169df8fd1ededde8b
ljx <1909129771@qq.com>
2023-12-03 19:38:22 +0800

    fix

--- sql/update.sql
+++ sql/update.sql
@@ -6,7 +6,7 @@ CREATE TABLE `update`
     `content`  varchar(1024) NOT NULL COMMENT '正文',
     `status`   int           NOT NULL COMMENT '状态码,状态码：0-未通过审核；1-通过审核；2-已下架',
     `type`     int           NOT NULL COMMENT '动态类型',
-    `dateTime` timestamp     NOT NULL COMMENT '时间',
+    `upload_time` timestamp     NOT NULL COMMENT '时间',
     `urls`     varchar(1024) DEFAULT NULL COMMENT '多媒体urls的json',
     `pid`      int           NOT NULL COMMENT '分区id',
     PRIMARY KEY (`id`),
